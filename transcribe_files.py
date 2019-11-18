2b231c3455149a28db9a45e023a316789cdd1ef6
Esteban Buz <esteban.buz@gmail.com>
2019-11-18 17:50:23 -0500

    adding example to transcriber and change to the script

--- transcribe_files.py
+++ transcribe_files.py
@@ -15,12 +15,22 @@ from cloud_transcribers import azure_transcribe_file, google_transcribe_file, wa
 
 parser = argparse.ArgumentParser()
 parser.add_argument("audio_files_path", help = "The path to the audio files directory")
-parser.add_argument("csv_output", help = "csv output file name")
-parser.add_argument("pickle_output", help = "pickle output file name")
+parser.add_argument("--audio_file_prefix", default = '', help = "prefix to include in glob for audiofiles")
+parser.add_argument("--csv_output", default = 'transcripts.csv', help = "csv output file name")
+parser.add_argument("--pickle_output", default = 'transcripts.pickle', help = "pickle output file name")
+
+parser.add_argument("--services",
+        nargs = '+',
+        default = ['azure', 'google', 'watson'],
+        choices = ['azure', 'google', 'watson'],
+        help = "Service(s) to use for transcription")
+
+parser.add_argument("--dry_run", action = 'store_true', help = "Just dry run, do not call cloud apis")
 
 args = parser.parse_args()
 
-wav_files = glob.glob(os.path.join(args.audio_files_path, '*.wav' ))
+
+wav_files = glob.glob(os.path.join(args.audio_files_path, args.audio_file_prefix + '*.wav' ))
 
 csv_file = open(args.csv_output, 'w')
 csv_writer = csv.DictWriter(csv_file,
@@ -35,16 +45,21 @@ pickle_writer = pickle.Pickler(pickle_file,
 
 transcribers = {
         'google': google_transcribe_file,
-        # 'azure': azure_transcribe_file,
+        'azure': azure_transcribe_file,
         'watson': watson_transcribe_file
         }
 
 for wav in wav_files:
     speech_binary = io.open(wav, 'rb').read()
     for name,method in transcribers.items():
+        if name not in args.services:
+            continue
         try:
             result = {}
-            result = method(speech_binary = speech_binary)
+            if args.dry_run:
+                result = {'service': name}
+            else:
+                result = method(speech_binary = speech_binary)
             csv_writer.writerow({'filename': wav, **result})
             pickle_writer.dump({'filename': wav, **result})
         except Exception as e:
